e27eab520b5ec105929939f2c3561315e4886757
Алексей <setgecko@gmail.com>
2023-03-21 15:11:40 +0300

    Выполнил задание 9.1.3

---
+++ Task_9.1.3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_9._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception();
+
+            exception.Data.Add("Дата создания исключения: ",DateTime.Now);
+        }
+    }
+}
