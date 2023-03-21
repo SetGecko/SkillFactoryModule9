bbfcd0e567671ad655dae275d06d2ed77bae3b53
Алексей <setgecko@gmail.com>
2023-03-21 15:13:54 +0300

    Выполнил задание 9.1.4

---
+++ Task_9.1.4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_9._1._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception exception = new Exception("Произошло какое-то исключение");
+
+            exception.HelpLink = "www.pikabu.ru";
+        }
+    }
+}
