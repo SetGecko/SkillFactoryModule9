c55b79bee9c01844b674faf92c1a1cf4f0db2cb4
Алексей <setgecko@gmail.com>
2023-03-21 15:09:21 +0300

    Таблица исключений 9.1

--- Exceptions_9.1/Program.cs
+++ Exceptions_9.1/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            /*
+             * Исключение	Условие
+                ArgumentException	        Непустой аргумент, передаваемый в метод, является недопустимым.
+                ArgumentNullException	    Аргумент, передаваемый в метод — null.
+                ArgumentOutOfRangeException	Аргумент находится за пределами диапазона допустимых значений.
+                DirectoryNotFoundException	Недопустимая часть пути к каталогу.
+                DivideByZeroException	    Знаменатель в операции деления или целого числа Decimal равен нулю.
+                DriveNotFoundException	    Диск недоступен или не существует.
+                FileNotFoundException	    Файл не существует.
+                FormatException	            Значение не находится в соответствующем формате для преобразования из строки методом преобразования, например Parse .
+                IndexOutOfRangeException	Индекс находится за пределами границ массива или коллекции.
+                InvalidOperationException	Вызов метода недопустим в текущем состоянии объекта.
+                KeyNotFoundException	    Не удается найти указанный ключ для доступа к элементу в коллекции.
+                NotImplementedException	    Метод или операция не реализованы.
+                NotSupportedException	    Метод или операция не поддерживается.
+                ObjectDisposedException	    Операция выполняется над объектом, который был ликвидирован.
+                OverflowException	        Арифметическое, приведение или операция преобразования приводят к переполнению.
+                PathTooLongException	    Длина пути или имени файла превышает максимальную длину, определенную системой.
+                PlatformNotSupportedException	Операция не поддерживается на текущей платформе.
+                RankException	            В метод передается массив с неправильным числом измерений.
+                TimeoutException	        Срок действия интервала времени, выделенного для операции, истек.
+                UriFormatException	        Используется недопустимый универсальный код ресурса (URI).
+            */
         }
     }
 }
