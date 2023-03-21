a094aca56c5a8af4466b501dc677eb172adbcf42
Алексей <setgecko@gmail.com>
2023-03-21 15:49:16 +0300

    Выполнил задание 9.2.2

---
+++ Task_9.2.2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_9._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArgumentOutOfRangeException("Аргумент находится за пределами диапазона допустимых значений.");
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally.");
+            }
+        }
+    }
+}
