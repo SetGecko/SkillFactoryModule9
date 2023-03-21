b2817c9c9d615f0d26395adc776c91d3b42e7b44
Алексей <setgecko@gmail.com>
2023-03-21 15:52:18 +0300

    Выполнил задание 9.2.3

---
+++ Task_9.2.3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_9._2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new RankException("Какое-то сообщение");
+            }
+            catch(RankException ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally.");
+            }
+        }
+    }
+}
