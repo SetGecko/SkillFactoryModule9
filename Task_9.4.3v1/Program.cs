2f1940fd6937e82885c54df55ebf312d03556e1d
Алексей <setgecko@gmail.com>
2023-03-22 11:11:07 +0300

    Выполнил задание 9.4.3

---
+++ Task_9.4.3v1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_9._4._3v1
+{
+    internal class Program
+    {
+        public delegate Parent HandlerMethod();
+        delegate void ChildInfo(Child child);
+        static void Main(string[] args)
+        {
+            ChildInfo childInfo = GetParentInfo;
+
+            childInfo.Invoke(new Child());
+        }
+        public static Parent ParentHandler()
+        {
+            return null;
+        }
+        public static Child ChildHandler()
+        {
+            return null;
+        }
+        public static void GetParentInfo(Parent p)
+        {
+            Console.WriteLine(p.GetType());
+        }
+    }
+    class Parent { }
+    class Child : Parent { }
+}
