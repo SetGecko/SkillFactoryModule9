9b7214166c8796125ab0b81a369f0cecdbd6e28a
Алексей <setgecko@gmail.com>
2023-03-22 09:32:13 +0300

    Практикак лябжа выражения

---
+++ PracticeLamdaExpressions_9.3/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PracticeLamdaExpressions_9._3
+{
+    internal class Program
+    {
+        delegate int Calculate(int a, int c);
+        static void Main(string[] args)
+        {
+            Calculate calculation = (x, y) => x + y;
+            Console.WriteLine(calculation(10, 20));
+            Console.WriteLine(calculation(40, 20));
+        }
+    }
+}
