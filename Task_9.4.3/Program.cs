961a90db46563a98594fc37d66ef64a1ca5c9511
Алексей <setgecko@gmail.com>
2023-03-22 11:06:13 +0300

    Выполнил задание 9.4.2

---
+++ Task_9.4.3/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_9._4._2
+{
+    internal class Program
+    {
+        public delegate Car HandlerMethod();
+        static void Main(string[] args)
+        {
+            HandlerMethod lexusHandler = LexusHandler;
+        }
+        public static Car CarHandler()
+        {
+            return null;
+        }
+        public static Lexus LexusHandler()
+        {
+            return null;
+        }
+    }
+    class Car { }
+    class Lexus : Car { }
+}
