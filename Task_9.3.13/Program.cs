029647b1b0c5bc9542a6bffeb20e68852fd7a299
Алексей <setgecko@gmail.com>
2023-03-22 09:30:51 +0300

    Выполнил задание 9.3.13

---
+++ Task_9.3.13/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_9._3._13
+{
+    internal class Program
+    {
+        delegate int RandomNumberDelegate();
+        static void Main(string[] args)
+        {
+            RandomNumberDelegate randomNumberDelegate = delegate
+            {
+                return new Random().Next(0, 100);
+            };
+            int result = randomNumberDelegate.Invoke();
+            Console.WriteLine(result);
+        }
+    }
+}
