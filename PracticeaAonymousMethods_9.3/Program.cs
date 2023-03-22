edecaeb7d618ab0744b779f824437ba0848252c2
Алексей <setgecko@gmail.com>
2023-03-22 09:26:14 +0300

    Практика по анонимным методам

---
+++ PracticeaAonymousMethods_9.3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PracticeaAonymousMethods_9._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GreetingsDelegate gd = delegate (string name)
+            {
+                return "Привет @" + name + " и добро пожаловать на SkillFactory!";
+            };
+            string GreetingsMessage = gd.Invoke("Pranaya");
+            Console.WriteLine(GreetingsMessage);
+
+
+            CalculateDelegate calculateDelegate = delegate (int a, int b)
+            {
+                return a + b;
+            };
+
+            int result = calculateDelegate.Invoke(50, 10);
+            Console.WriteLine(result);
+        }
+        public delegate string GreetingsDelegate(string name);
+        delegate int CalculateDelegate(int a, int b);
+    }
+}
