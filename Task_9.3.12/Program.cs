6169c5efc5a10fa474a939386f61635e5e5b00ff
Алексей <setgecko@gmail.com>
2023-03-22 09:28:18 +0300

    Выполнил задание 9.3.12

---
+++ Task_9.3.12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_9._3._12
+{
+    internal class Program
+    {
+        delegate void ShowMessageDelegate(string _message);
+        static void Main(string[] args)
+        {
+            ShowMessageDelegate showMessageDelegate = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
+        }
+    }
+}
