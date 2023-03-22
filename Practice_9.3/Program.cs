f618ed54e60677c7645f7329963bc7d54a0e4be5
Алексей <setgecko@gmail.com>
2023-03-22 07:02:22 +0300

    Практика 9.3

---
+++ Practice_9.3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice_9._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SumDelegate sumDelegate = Sum;
+            sumDelegate.Invoke(1, 10, 50);
+        }
+        static int Sum (int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public delegate int SumDelegate(int a, int b, int c);
+    }
+}
