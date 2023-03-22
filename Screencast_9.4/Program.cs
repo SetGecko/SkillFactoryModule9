fe46f67ee13c6f76804ff6564bed801916b6e4b5
Алексей <setgecko@gmail.com>
2023-03-22 11:01:25 +0300

    Практика по скринкасту 9.4

---
+++ Screencast_9.4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Screencast_9._4
+{
+    internal class Program
+    {
+        public delegate Animal HandlerMethod();
+        delegate void DogInfo(Dog dog);
+        public static Animal AnimalHandler()
+        {
+            return null;
+        }
+        public static Dog DogHandler()
+        {
+            return null;
+        }
+        public static void GetAnimalInfo(Animal p)
+        {
+            Console.WriteLine(p.GetType());
+        }
+        static void Main(string[] args)
+        {
+            HandlerMethod handlerMethod = AnimalHandler;
+            HandlerMethod handlerDog = DogHandler;
+
+            DogInfo dogInfo = GetAnimalInfo;
+            dogInfo.Invoke(new Dog());
+        }
+    }
+    class Animal { }
+    class Dog : Animal { }
+}
