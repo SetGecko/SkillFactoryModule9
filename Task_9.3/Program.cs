c02678db3b509fd429a1c6a40ea0fd8cc3e8f1d7
Алексей <setgecko@gmail.com>
2023-03-22 07:09:31 +0300

    Выполнил задание 9.3.2

--- Task_9.3/Program.cs
+++ Task_9.3/Program.cs
@@ -2,9 +2,19 @@
 {
     internal class Program
     {
+        /*Создайте консольное приложение, в котором есть функция, принимающая на вход два числа int, 
+         * и возвращающая результат int вычитания второго числа из первого. 
+         * Вызовите эту функцию в классе Main при помощи делегата и отобразите результат в консольном сообщении.*/
         static void Main(string[] args)
         {
-            
+            SubtractionDelegate subtractionDelegate = Subtraction;
+            int result = subtractionDelegate(50, 40);
+            Console.WriteLine(result);
         }
+        static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        public delegate int SubtractionDelegate(int a, int b);
     }
 }
